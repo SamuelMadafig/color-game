3ecf7579e5b39e82ab65b437bff5197004de777c
Samuel <test@test.com>
2019-02-09 17:00:23 +1100

    Updates

---
+++ precache-manifest.0c9f184dbe9832968caf77cdd2b7f8bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5876cbcfb5e175788b33",
+    "url": "/color-game/static/js/runtime~main.5876cbcf.js"
+  },
+  {
+    "revision": "683ee3a603ca579ab4a7",
+    "url": "/color-game/static/js/main.683ee3a6.chunk.js"
+  },
+  {
+    "revision": "3a3069a6515818fb9ad1",
+    "url": "/color-game/static/js/1.3a3069a6.chunk.js"
+  },
+  {
+    "revision": "683ee3a603ca579ab4a7",
+    "url": "/color-game/static/css/main.968719a5.chunk.css"
+  },
+  {
+    "revision": "cd5fef3f3c7a92ddbe87459a80fd6d9d",
+    "url": "/color-game/index.html"
+  }
+];
